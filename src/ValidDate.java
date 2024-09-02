13e8c975eba1659ed4841d33726b1b7b19a195de
Saichandu090 <marrisaichandu143@gmail.com>
2024-09-02 21:43:50 +0530

    Modified Valid Date program

--- src/ValidDate.java
+++ src/ValidDate.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class ValidDate
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the date");
         int date=sc.nextInt();
